05ed67a005c3d12d8b7a4b95d9b7dd99341a2e28
Saksham-garg <sakshamgarg142@hotmail.com>
2024-06-29 20:18:02 +0530

    added user schema

---
+++ app/schemas.py
@@ -0,0 +1,42 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+
+class PostBase(BaseModel):
+    title: str
+    content: str    
+    published: bool = False
+
+
+class PostCreate(PostBase):
+    pass
+
+
+class Post(PostBase):
+    id:int
+    created_at:datetime
+
+    class Config:
+        orm_mode = True
+
+
+
+
+class UserCreate(BaseModel):
+    email:EmailStr
+    password:str
+
+
+class User(BaseModel):
+    id:int
+    created_at:datetime
+
+    class Config:   
+        orm_mode:True
+
+class UserOut(BaseModel):
+    id:int
+    email:EmailStr
+    created_at:datetime
+
+    class Config:
+        orm_mode = True
